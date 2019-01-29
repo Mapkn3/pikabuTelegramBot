cfb18aa6124dcc2a991ad291eecb39d003630508
Mapkn3 <79608236848@ya.ru>
2019-01-30 02:34:37 +0400

    First test bot

--- src/main/java/my/mapkn3/pikabuTelegramBot/Bot.java
+++ src/main/java/my/mapkn3/pikabuTelegramBot/Bot.java
@@ -16,9 +16,11 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdatesReceived(List<Update> updates) {
-        if (updates.get(0).getMessage().getCaption() != null) {
-            if (updates.get(0).getMessage().getCaption().charAt(0) == '#') {
+        String caption = updates.get(0).getMessage().getCaption();
+        if (caption != null) {
+            if (caption.charAt(0) == '#') {
                 isActive = true;
+                hashtag = caption;
             }
         }
         updates.forEach(this::onUpdateReceived);
@@ -42,11 +44,6 @@ public class Bot extends TelegramLongPollingBot {
             }
             System.out.println("Has photo? " + message.hasPhoto());
             if (message.hasPhoto()) {
-                if (message.getCaption() != null) {
-                    hashtag = message.getText();
-                    isActive = true;
-                    System.out.println("Hashtag change to " + hashtag);
-                }
                 SendPhoto sendPhoto = new SendPhoto();
                 sendPhoto.setChatId(message.getChatId());
                 sendPhoto.setCaption(hashtag);
