b4939629c6c61738ead2278e7d8009e120898ad5
Mapkn3 <79608236848@ya.ru>
2019-02-01 23:02:52 +0400

    First test bot

--- src/main/java/my/mapkn3/pikabuTelegramBot/Bot.java
+++ src/main/java/my/mapkn3/pikabuTelegramBot/Bot.java
@@ -2,6 +2,7 @@ package my.mapkn3.pikabuTelegramBot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendAnimation;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -16,6 +17,7 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdatesReceived(List<Update> updates) {
+        System.out.println("Get " + updates.size() + " updates. Is active: " + isActive);
         boolean allInOne = false;
         String caption = updates.get(0).getMessage().getCaption();
         if (caption != null) {
@@ -67,6 +69,17 @@ public class Bot extends TelegramLongPollingBot {
                 if (isActive) {
                     System.out.println("Send animation");
                     execute(sendAnimation);
+                }
+            }
+            System.out.println("Has document? " + message.hasDocument());
+            if (message.hasDocument()) {
+                SendDocument sendDocument = new SendDocument();
+                sendDocument.setChatId(message.getChatId());
+                sendDocument.setCaption(hashtag);
+                sendDocument.setDocument(message.getDocument().getFileId());
+                if (isActive) {
+                    System.out.println("Send document");
+                    execute(sendDocument);
                 }
             }
             if (isActive) {
